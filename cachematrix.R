f514ec52f1dd095fe359e1418e4eecebdd8f6590
mlachut <mlachut235au@gmail.com>
2015-01-21 03:18:47 +1100

    added midified makeVector function from example

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function below will allow to define a matrix and output functions to store its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  Inv <- NULL
+  set <- function(y) {
+    x <<- y
+    Inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(Inv) Inv <<- solve(x)
+  getInv <- function() Inv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
-##hellohello
+  
+}
+
 
 ## Write a short comment describing this function
 
