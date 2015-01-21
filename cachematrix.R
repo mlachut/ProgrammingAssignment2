2c7e5c720e037ca0e9f402fb818f60af2fedb46a
mlachut <mlachut235au@gmail.com>
2015-01-22 00:28:50 +1100

    added and modified comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## A matrix is input into the function below matrix which outputs function that allow its inverse to be cached within 
+## A matrix is input into the function below which outputs function that allow its inverse to be cached within 
 ## the a 'special' matrix
-## Please note that the makeVector function from the example was modified accordingly to impliment the above procedure
+## Please note that the makeVector function from the example was modified accordingly to impliment the procedure described 
+## above
   
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -25,12 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## The function below will check if the input matrix has its inverse cached and returns it if TRUE
 ## Otherwise, it will solve for the inverse of the matrix, cache it into the matrix with setInv() and output the result
-## Please note that the cachemean function from the example was modified accordingly to impliment the above procedure
+## Please note that the cachemean function from the example was modified accordingly to impliment the procedure described 
+## above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
-  if (nrow(x$get()) == ncol(x$get())) {
+  if (nrow(x$get()) == ncol(x$get())) {  ##check to see if the input matrix is squares
     Ix <- x$getInv()
     if(!is.null(Ix)) {
       message("getting cached inverse of matrix")
